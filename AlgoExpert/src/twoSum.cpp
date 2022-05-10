243d846068f4b774f35e1158010efaf091222c12
Abhishek <tewari.abhishek.16@gmail.com>
2022-05-10 20:07:45 +0530

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    You may assume that each input would have exactly one solution, and you
    may not use the same element twice.
    
    You can return the answer in any o

--- AlgoExpert/src/twoSum.cpp
+++ AlgoExpert/src/twoSum.cpp
@@ -6,5 +6,32 @@
  */
 
 
+#include <vector>
+#include <set>
+using namespace std;
 
+vector<int> twoNumberSum(vector<int> array, int targetSum) {
+  // Write your code here.
+	vector<int> twoSum;
+	set<int> collectElem;
+
+	for(int i = 0; i < array.size(); i++)
+	{
+			for (auto it = collectElem.begin(); it != collectElem.end(); it++)
+			{
+					if((*it != array[i]) && ((*it + array[i]) == targetSum))
+						 {
+							 	twoSum.push_back(array[i]);
+							  twoSum.push_back(*it);
+						 }
+			}
+			collectElem.insert(array[i]);
+	}
+  return twoSum;
+}
+
+int main()
+{
+	return 0;
+}
 
